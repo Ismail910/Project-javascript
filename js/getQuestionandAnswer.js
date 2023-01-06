71d7fcc5702fb7d6c914d719bc87a83f37db3db0
OmarGamal07 <omar.gamal.dev@gmail.com>
2023-01-06 18:06:12 +0200

    make drag and drop answers

--- js/getQuestionandAnswer.js
+++ js/getQuestionandAnswer.js
@@ -1,16 +1,30 @@
 var dataAfterConvert;
 var id=1,idputans=10,idque=20;
 var ansobj;
-function drag(ansget) {
-    ansobj=ansget;
+// function drag(ansget) {
+//     ansobj=ansget;
+//     console.log(ansobj);
+//   }
+function drag(ev) {
+    ev.dataTransfer.setData("text", ev.target.id);
+    var data = ev.dataTransfer.getData("text");
+    console.log(data);
+  }
+  function drop(ev) {
+    ev.preventDefault();
+    var data = ev.dataTransfer.getData("text");
+    console.log(data);
+    ev.target.appendChild(document.getElementById(data));
   }
   function allowDrop(ev) {
     ev.preventDefault();
   }
-  function DropElement(elem){
-     var x=document.getElementById(elem);
-      x.appendChild=(ansobj);
-}
+//   function DropElement(elem){
+//     // var x=document.getElementById(elem);
+//       elem.appendChild=(ansobj);
+//       console.log(ansobj);
+//       console.log(elem);
+// }
 function getData(){
     var xhttp= new XMLHttpRequest();
     xhttp.onreadystatechange=function(){
@@ -20,7 +34,7 @@ function getData(){
             //console.log(dataAfterConvert);
             for(var i=0;i<dataAfterConvert.length;i++)
             {
-                console.log(dataAfterConvert[i]['level']);
+                //console.log(dataAfterConvert[i]['level']);
                 if(dataAfterConvert[i]['level']==sessionStorage.getItem('currentlevel'))
                 {
                    // document.getElementById("head")=dataAfterConvert[i]["name"];
@@ -29,28 +43,36 @@ function getData(){
                     //div contains answers to drag in correct answer filed
                     const para = document.createElement('p');
                     const node = document.createTextNode(dataAfterConvert[i]['answers'][j].answer);
-                    para.id=id++;
                     para.appendChild(node);
+                    para.id="asd"+id++;
                     document.getElementById("head").appendChild(para);
                     para.draggable=true;
-                    para.ondragstart=drag(para.textContent);
+                   
+                    console.log(ansobj)
+                    console.log(document.getElementById(para.id));
                     
                     //field to show questions
                     const paraque = document.createElement('p');
                     const nodeque = document.createTextNode(dataAfterConvert[i]['questions'][j].question);
                     paraque.id=idque++;
+                    //console.log(paraque.id)
                     paraque.appendChild(nodeque);
                     document.getElementById("Quiz").appendChild(paraque);
+
+                    
                     //field to drop answer
-                    const paraans = document.createElement('div');
-                    const nodeans = document.createTextNode("");
-                    paraans.id=idputans++;
-                    paraans.style.width="250px";
-                    paraans.style.height="40px";
-                    paraans.appendChild(nodeans);
-                    document.getElementById("answers").appendChild(paraans);
-                    paraans.ondragover=event.preventDefault();
-                    paraans.ondrop=DropElement(paraans.id);   
+                    // const paraans = document.createElement('div');
+                    // const nodeans = document.createTextNode("");
+                    // paraans.id=idputans++;
+                    // paraans.style.width="250px";
+                    // paraans.style.backgroundColor="red";
+                    // paraans.style.height="40px";
+                    // paraans.appendChild(nodeans);
+                    // document.body.appendChild(paraans);
+                    // //paraans.ondragover=allowDrop(event);
+                    // paraans.ondrop=DropElement(paraans.id);   
+                    // document.getElementById("answers").ondragover=event.preventDefault();
+                    // document.getElementById("answers").ondrop=document.getElementById("answers").appendChild=(ansobj); 
                    }
                 }
             }  
@@ -63,3 +85,5 @@ function getData(){
 
 }
 getData();
+
+//document.getElementById("asd1").ondragstart=drag(document.getElementById("asd1"));
